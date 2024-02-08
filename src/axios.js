b347226b182626ff688410e30bab32db3573965e
Anju Sebastian <christeenaanju@gmail.com>
2024-02-08 12:59:43 +0530

    updated render url

--- src/axios.js
+++ src/axios.js
@@ -2,6 +2,6 @@ import axios from "axios";
 
 
 const instance=axios.create({
-    baseURL:"http://localhost:4000/"
+    baseURL:"https://smart-backend-render.onrender.com/"
 })
 export default instance;
